4b71ba819086b86d9c48aa774b77ace0dfb8c442
Cameron Wright <ccameronwright@gmail.com>
2022-06-19 23:35:07 -0600

    Rebuilt the entire simpleInstructor to be more DRY, use more imported internal libraries.

--- simpleHack/simpleInstructor.js
+++ simpleHack/simpleInstructor.js
@@ -1,78 +1,151 @@
+import { cliArgDict, checkForArg } from "/lib/cliArgs";
 import { getAllKnownServers } from "/lib/findAllServersDFS.js"
+import { freeRam, getLargestRamUsage } from "/lib/MemoryProcs.js"
+import { Logger } from "/lib/Logger.js"
 
 /** @param {NS} ns */
 export async function main(ns) {
+    // simpleInstructor.js <targetSystem> [destination] [ignoreHome] [minMoneyPercent] [freeRam] [killExisting]
+    //ns.disableLog("ALL");
+
+    //Command Line Arguments
+    let argDict = cliArgDict(ns.args);
+    let dest = checkForArg(argDict, 'dest', null)
+    let target = checkForArg(argDict, 'target', null)
+    let minMoneyPercent = checkForArg(argDict, 'minMoneyPercent', 0.75)
+    let ignoreHome = checkForArg(argDict, 'ignoreHome', false)
+    let freeRam = checkForArg(argDict, 'freeRam', 0)
+    let killExisting = checkForArg(argDict, 'killExisting', false)
     
-    ns.disableLog("ALL");
+    let logger = new Logger(ns, true, true);
 
-    // Script in charge of deploying simpleDeploy on all systems
-    if(ns.args.length < 1) {
-        ns.tprint("simpleInstructor.js [targetSystem] [ignoreHome] [minMoneyPercent]")
-        // ns.exit()
+    //Information on hacking scripts, and args tp pass to the next one.
+    let scriptInfo = {
+        entryPoint: '/simpleHack/simpleSurvey.js',
+        args: [target, minMoneyPercent],
+        files: ['/simpleHack/simpleHack.js', '/simpleHack/simpleSurvey.js']
     }
 
-    let targetSystem = (ns.args.length >= 1) ? ns.args[0] : null;
-    let ignoreHome = (ns.args.length >= 2) ? ns.args[1] : true;
-    let minMoneyPercent = (ns.args.length >= 3) ? ns.args[2] : 0.75;
+    //Data collection on server deployment pass/fail
+    let metaData = {
+        pidCount : 0,
+        RAMIssues: { 'count': 0, 'servers': [] },
+        rootIssues: { 'count': 0, 'servers': [] }
+    }
 
-    let serverList = await getAllKnownServers(ns);
-    let pidCount = 0;
-    let ramIssueCount = 0;
-    let rootIssueCount = 0;
-    ns.disableLog("ALL");
-    //Loop through all known servers, deciding how to deploy the script
-    for(let i = 0; i < serverList.length; i++) {
-        let server = serverList[i];
-        let pid;
-        //Before trying to execute script, check if target system has enough RAM, has root access.
-        // TODO If statement assumes that scriptSurvey is larger than scriptHack, which it is but thats faith.
-        if(!ns.hasRootAccess(server)) {
-            ns.tprint(server + " Doesn't have root access Skipping...")
-            rootIssueCount++;
-            continue
-        }else if( (ns.getServerMaxRam(server) - ns.getServerUsedRam(server)) < ns.getScriptRam('/simpleHack/simpleSurvey.js')){
-            ns.tprint("Server " + server + " has insulficiant RAM Skipping...")
-            ns.tprint("Max RAM: " + ns.getServerMaxRam(server))
-            ns.tprint("RAM Used: " + ns.getServerUsedRam(server))
-            ramIssueCount++;
-            continue
+    //If destination provided, only push script to that system
+    if (dest) {
+        logger.info(`Setting up script at one location, ${dest}`)
+        await startDeployment(ns, logger, dest, scriptInfo, metaData);
+        printResults(ns, logger, 1, metaData);
+    } else {
+        logger.info(`Setting up script on all servers.`)
+        // Get All Known Servers, push to those
+        let serverList = await getAllKnownServers(ns);
+        for(let server of serverList) {
+            let pid;
+            await startDeployment(ns, logger, server, scriptInfo, metaData);
+        }
+        printResults(ns, logger, serverList.length, metaData);
+        // Get All Purchased Servers, deploy to those
+
+        // Buy more Purchased Servers, if can
+        // Deploy to Home or maybe set this script into daemon mode to not quite.
+    }
+}
+
+export async function startDeployment(ns, logger, server, scriptInfo, metaData) {
+    try {
+        //Check if it has root access,
+        if (!ns.hasRootAccess(server)) {
+            metaData.rootIssues.count++;
+            metaData.rootIssues.servers.push(server);
+            //TODO Call the breach script, it could handle this issue
+            return
         }
 
-        //Check that current system has enough RAM to fork the deploy script.
-        let checks = 0;
-        const MAX_CHECKS = 5;
-        
-        // let expectedLoad = ns.getScriptRam('/simpleHack/simpleInstructor.js') + ns.getScriptRam('/simpleHack/simpleDeploy.js');
-        let expectedLoad = ns.getScriptRam('/simpleHack/simpleDeploy.js');
-        do {
-            //Expected load is this server plus simpleDeploy.js
-            let currentFree = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
-            if(currentFree >= expectedLoad ) {
-                break;
-            }
-            ns.print("Not enough free RAM  " + currentFree + ". Need " + expectedLoad + "Sleeping for 20 seconds.")
-            await ns.sleep(1000 * 20) // 20 seconds 
-            checks++;
+        //Check RAM
+        let exitCode = checkRAM(ns, server, scriptInfo.files, metaData);
+        if (exitCode) {
+            makeRoom(ns, server) //Does Nothing Right now
+            return
         }
-        while (checks <= MAX_CHECKS);
+        //Deploy script to server, unless our destinartion is this server
+        logger.info(`Copying files ${scriptInfo.files}, over to ${server}`);
+        await ns.scp(scriptInfo.files, server);
 
+        // calculateThreads()
+        let threads = 1;
+
+        //Kick Off
+        logger.info("Kicking off run")
+        await startScript(ns, logger, server, threads, scriptInfo, metaData);
+        logger.info("Finished setting up script.")
+    } catch (error) {
+        logger.info(`Exception thrown, error ${error}`)
+    }
+}
+
+export function checkRAM(ns, server, files, metaData) {
+    let exitCode = 0;
+    if (freeRam(ns, server) < getLargestRamUsage(ns, server, files)) {
+        metaData.RAMIssues.count++;
+        metaData.RAMIssues.servers.push(server);
+        exitCode = -1;
+    }
+    return exitCode;
+}
+
+export async function startScript(ns, logger, host, threads, scriptInfo, metaData) {
+    try{
+        const args = scriptInfo.args
         // If no target system is selected tell script to hack host
-        if (targetSystem == null){
-            ns.run("/simpleHack/simpleDeploy.js", 1, server, targetSystem, minMoneyPercent);
+        logger.info(`starting ${scriptInfo.entryPoint} on ${host}`);
+        let pid = await ns.exec(scriptInfo.entryPoint, host, threads, ...args);
+        if (!pid) {
+            logger.info(`Error starting /simpleHack/simpleDeploy.js script. pid: ${pid}`);
         } else {
-            ns.run("/simpleHack/simpleDeploy.js", 1, server, targetSystem, minMoneyPercent);
+            metaData.pidCount++;
         }
-        // Can't do anything waiting for pids yet, but can count how many were successful
-        if (pid){
-            ns.print("Error starting /simpleHack/simpleDeploy.js script")
-        } else{
-            pidCount++;
-        }
+    }catch (error){
+        logger.info(`Exception: Executing script, ${error}`)
     }
-    if(!ignoreHome) {
-        serverList.push('home')
-        // TODO terminate this script and setup the deploy on this server, it will need to be pointed at a target
+}
+
+export function makeRoom(ns, server) {
+    // //Check that current system has enough RAM to fork the deploy script.
+    // let checks = 0;
+    // const MAX_CHECKS = 5;
+
+    // // let expectedLoad = ns.getScriptRam('/simpleHack/simpleInstructor.js') + ns.getScriptRam('/simpleHack/simpleDeploy.js');
+    // let expectedLoad = ns.getScriptRam('/simpleHack/simpleDeploy.js');
+    // do {
+    //     //Expected load is this server plus simpleDeploy.js
+    //     let currentFree = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
+    //     if(currentFree >= expectedLoad ) {
+    //         break;
+    //     }
+    //     logger.info("Not enough free RAM  " + currentFree + ". Need " + expectedLoad + "Sleeping for 20 seconds.")
+    //     await ns.sleep(1000 * 20) // 20 seconds 
+    //     checks++;
+    // }
+    // while (checks <= MAX_CHECKS);
+    return True
+}
+
+export function printResults(ns, logger, serverCount, metaData) {
+    try{
+        const rootIssueCount = metaData.rootIssues.count;
+        const ramIssueCount = metaData.RAMIssues.count;
+        const rootIssuesServers = metaData.rootIssues.servers;
+        const ramIssuesServers = metaData.RAMIssues.servers;
+        const pidCount = metaData.pidCount;
+
+        logger.info(`Of the ${serverCount} servers found succuessfully deployed to ${pidCount}`);
+        logger.info(`Not root on ${rootIssueCount}, Not enough Memory found on ${ramIssueCount}`);
+        logger.info(`Servers with root issues were: ${rootIssuesServers}`)
+        logger.info(`Servers with memory issues were: ${ramIssuesServers}`)
+    } catch (error){
+        logger.info(`Exception Thrown trying to print results: ${error}`)
     }
-    ns.tprint("Of the " + serverList.length + " servers found Succuessfully deployed to " + pidCount);
-    ns.tprint("NOt ROOT on " + rootIssueCount + ", Not enough Memory found on " + ramIssueCount);
 }
